5e1e24e2d0de15e28f66f3e69899a97913fe5030
rafiqulbd95 <100084312+rafiqulbd95@users.noreply.github.com>
2023-11-09 15:42:08 +0600

    Initial commit

---
+++ assets/js/main.js
@@ -0,0 +1,11 @@
+
+    $('.portfolio-grid').isotope({
+        itemSelector: '.portfolio-item',
+        percentPosition: true,
+        masonry: {
+            // use outer width of grid-sizer for columnWidth
+            columnWidth: 1,
+        }
+    });
+
+
